e3578de88fccbed1e62926c9ade464f188914a1d
Stephen Lovell <sjlovell34@gmail.com>
2015-08-16 19:34:09 -0400

    add surveys views for rails_admin

--- app/controllers/attempts_controller.rb
+++ app/controllers/attempts_controller.rb
@@ -1,46 +1,60 @@
 class AttemptsController < ApplicationController
 
-  helper 'surveys'
+    helper 'surveys'
 
-  before_filter :load_survey, only: [:new, :create]
+    before_filter :normalize_attempts_data, :only => :create
 
-  def show
-    @attempt = Survey::Attempt.find_by(id: params[:id])
-    render :access_error if current_user.id != @attempt.participant_id
-  end
-
-  def new
-    @participant = current_admin_user || current_user
-
-    unless @survey.nil?
-      @attempt = @survey.attempts.new
-      @attempt.answers.build
+    def new
+      @participant = current_user # you have to decide what to do here
+      # @participant = current_user || current_admin
+      
+      unless @survey.nil?
+        @attempt = @survey.attempts.new
+        @attempt.answers.build
+      end
     end
-  end
 
-
-  def create
-    @attempt = @survey.attempts.new(params_whitelist)
-    @attempt.participant = current_user
-    if @attempt.valid? && @attempt.save
-      correct_options_text = @survey.correct_options.present? ? 'Bellow are the correct answers marked in green' : ''
-      redirect_to attempt_path(@attempt.id), notice: "Thank you for answering #{@survey.name}! #{correct_options_text}"
-    else
-      build_flash(@attempt)
-      @participant = current_user
-      render :new
+    def create
+      @attempt = @survey.attempts.new(attempt_params)
+      @attempt.participant = current_user
+      if @attempt.valid? && @attempt.save
+        redirect_to view_context.new_attempt, alert: I18n.t("attempts_controller.#{action_name}")
+      else
+        render :action => :new
+      end
     end
-  end
-
-  def load_survey
-    @survey = Survey::Survey.find_by(id: params[:survey_id])
-  end
-
-  def params_whitelist
-    if params[:survey_attempt]
-      params[:survey_attempt][:answers_attributes] = params[:survey_attempt][:answers_attributes].map { |attrs| { question_id: attrs.first, option_id: attrs.last } }
+    private
+    def load_active_survey
+      @survey =  Survey::Survey.active.first
+    end
+    def normalize_attempts_data
+      normalize_data!(params[:survey_attempt][:answers_attributes])
+    end
+    def normalize_data!(hash)
+      multiple_answers = []
+      last_key = hash.keys.last.to_i
+      hash.keys.each do |k|
+        if hash[k]['option_id'].is_a?(Array)
+          if hash[k]['option_id'].size == 1
+            hash[k]['option_id'] = hash[k]['option_id'][0]
+            next
+          else
+            multiple_answers <<  k if hash[k]['option_id'].size > 1
+          end
+        end
+      end
+      multiple_answers.each do |k|
+        hash[k]['option_id'][1..-1].each do |o|
+          last_key += 1
+          hash[last_key.to_s] = hash[k].merge('option_id' => o)
+        end
+        hash[k]['option_id'] = hash[k]['option_id'].first
+      end
+    end
+    def attempt_params
+      rails4? ? params_whitelist : params[:survey_attempt]
+    end
+    def params_whitelist
       params.require(:survey_attempt).permit(Survey::Attempt::AccessibleAttributes)
     end
-  end
-
 end
